6f6724496997a8a1e40420cb59c695d5de3d3f6d
Aayush Anand <108023444+almanack007@users.noreply.github.com>
2025-10-14 00:39:26 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/aayush-anand-portfolio-muse/",
   server: {
     host: "::",
     port: 8080,
